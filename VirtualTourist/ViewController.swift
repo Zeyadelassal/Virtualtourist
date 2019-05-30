21e249c84485fa1678fb781bd107f34384789d3d
zeyadel3ssal <zeyadel3ssal@192.168.79.128>
2019-05-30 21:22:03 +0200

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by zeyadel3ssal on 5/30/19.
+//  Copyright © 2019 zeyadel3ssal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
